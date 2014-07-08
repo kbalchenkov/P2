78451c734d4c4336dfdc993bfaa049e6120bffeb
Kirill Balchenkov <kirill00k@gmail.com>
2014-07-08 00:56:26 -0400

    Random Picking is Working

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+error_reporting(E_ALL);         # Report Errors, Warnings, 
+ini_set('display_errors' , 1);   # Display errors on page (instead of a log file)
+ 
+?>
+
 
 <!DOCTYPE html>
 <html>
@@ -6,26 +12,66 @@
 
   
   <link rel='stylesheet' href='styles.css' type='text/css'>
+ 
+ <?php require 'listofwords.php' ?>
  <?php require 'logic.php' ?>
-  <?php require 'listofwords.php' ?>
+
+  
 
 </head>
-<body class="<?php echo $image; ?>">
 
-<h1> It is <?php echo $time_now	?> </h1>
-<h1>Time Zone <?php echo date_default_timezone_get();?></h1>
+<body>
 
+
+	<h1>xkcd style password generator</h1>
+ 
+<form method='POST' action='index.php'>
+
+# of words: <input name="numberofwords" id ="numberofwords" min="1" max="5" maxlength=1 type="number" required> (Max 5)<br>
 <br>
-<img src='http://making-the-internet.s3.amazonaws.com/php-<?php echo $image; ?>.png' alt='Scenery matching the time of day'>
 
+
+<input type='checkbox' name='addanumber' id='addanumber' ><label for='addanumber'>Add a number</label> <br>
+
+
+<input type='checkbox' name='addasymbol' id='addasymbol' > 
+<label for='addasymbol'>Add a symbol</label><br>
+ 
+<input type='checkbox' name='adduppercase' id='adduppercase' > 
+<label for='adduppercase'>Upper case the first letter</label><br><br>
+ 
+ 
+<input type='submit' value='Generate!'>
+ 
+</form>
+
+
+
+<pre>
+    <?php print_r($_POST); ?>
+</pre>
+
+
+
+
+<?php 
+      foreach($displaywords as $key => $value) { ?>
+		<?php echo $value; ?> + <br>
+		<?php }?>
+
+
+<pre>
+    <?php print_r($displaywords); ?>
+</pre>
 
 
 <?php  
-foreach($keywords as $keyword => $result) {
-    echo $keyword." is a ".$result.".<br>";
-}
-?>   
-  
-  
+	foreach($keywords as $keyword => $result) { ?>
+		<?php echo $keyword; ?> is a <?php echo $result;?> <br> 
+<?php } ?>
+
+ 
+ 
+
 </body>
 </html>
--- logic.php
+++ logic.php
@@ -1,5 +1,48 @@
-<?php  
-foreach($contestants as $contestant => $result) {
-    echo $contestant." is a ".$result.".<br>";
+
+
+
+<?php
+
+
+
+$imputnumber = $_POST["numberofwords"];
+
+$i = 0;
+
+while($i < $imputnumber){
+	
+	$rand_word = array_rand($keywords);
+
+	$rand_value = $keywords[$rand_word];
+	
+	$displaywords[] = $rand_value;
+	
+	$i++;
+
 }
-?>   
+
+?>
+
+<?php
+
+$numberselected = $_POST["addanumber"];
+
+If(
+
+$imputnumber = $_POST["numberofwords"];
+
+$i = 0;
+
+while($i < $imputnumber){
+	
+	$rand_word = array_rand($keywords);
+
+	$rand_value = $keywords[$rand_word];
+	
+	$displaywords[] = $rand_value;
+	
+	$i++;
+
+}
+
+?>
